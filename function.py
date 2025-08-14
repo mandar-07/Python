99a097f4a7090ec396d8250c8a1a2333fbb10555
mandar patil <mandarp1110@gmail.com>
2025-08-14 15:54:21 +0530

    default function was added

--- function.py
+++ function.py
@@ -9,3 +9,9 @@ print("Squared value:", result)
 #lambda function
 no = lambda x: x**2
 print(no(2))
+
+def student(firstname, lastname ='Mark', standard ='Fifth'):
+
+     print(firstname, lastname, 'studies in', standard, 'Standard')
+
+student("mandar" , "patil")
